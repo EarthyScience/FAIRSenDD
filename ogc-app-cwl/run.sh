0497867a40dc895707bf809c4ddf734ac8502904
Daniel Loos <dloos@bgc-jena.mpg.de>
2025-03-05 11:14:36 +0100

    Add stac

--- ogc-app-cwl/run.sh
+++ ogc-app-cwl/run.sh
@@ -1,4 +1,14 @@
 #! /usr/bin/env sh
 
-docker build RQADeforestation.jl -t danlooo/rqa_deforestation:latest
-cwltool fairsendd.cwl#rqa --continent EU --tiles E051N018T3 --in-dir ../RQADeforestationTestData
+export TAG=latest
+docker compose build
+
+cwltool fairsendd.cwl#rqa \
+    --continent EU \
+    --tiles E051N018T3 \
+    --start-date 2021-01-01 \
+    --end-date 2022-01-01 \
+    --in-dir ../RQADeforestationTestData \
+    --out-dir test1 \
+    --access-key $AWS_ACCESS_KEY_ID \
+    --secret-key $AWS_SECRET_ACCESS_KEY
