3f3e2186d8ca7495a17951f2cdfa89a0244367ff
danlooo <dloos@bgc-jena.mpg.de>
2025-12-05 11:42:39 +0100

    Remove bootstrap

--- website/.vitepress/theme/index.ts
+++ website/.vitepress/theme/index.ts
@@ -1,10 +1,4 @@
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
-// overwrite bootstrap css
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
-
 
 export default DefaultTheme
